96c489d8daf6fb21959524d7fa40959e9fe7729e
Xudong Huang <huangxu008@hotmail.com>
2018-01-23 06:04:42 +0000

    :pencil: add necessary dependent libraries

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#[macro_use]
+extern crate log;
+extern crate http;
+extern crate httparse;
+extern crate may;
+extern crate time;
+
+pub mod client;
+pub mod server;
+
 #[cfg(test)]
 mod tests {
     #[test]
