ee57c25867827026eb005e0a531fdcbc769b5959
Xudong Huang <huangxu008@hotmail.com>
2018-01-26 14:17:28 +0000

    :pencil: impl SizedReader

--- src/body/body_reader.rs
+++ src/body/body_reader.rs
@@ -23,13 +23,38 @@ impl fmt::Debug for BodyReader {
 
 impl Read for BodyReader {
     #[inline]
-    fn read(&mut self, _msg: &mut [u8]) -> io::Result<usize> {
-        unimplemented!()
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        use std::cmp;
+        match *self {
+            SizedReader(ref r, ref mut remain) => {
+                let len = cmp::min(*remain, buf.len());
+                if len == 0 {
+                    return Ok(0);
+                }
+                let r = unsafe { &mut *(r.as_ref() as *const _ as *mut Read) };
+                let n = r.read(&mut buf[0..len])?;
+                *remain -= n;
+                Ok(n)
+            }
+            ChunkReader(ref _r) => unimplemented!(),
+            EmptyReader => Ok(0),
+        }
     }
 }
 
 impl Drop for BodyReader {
     fn drop(&mut self) {
-        // unimplemented!()
+        match *self {
+            SizedReader(ref r, remain) => {
+                // read enough data
+                if remain > 0 {
+                    let r = unsafe { &mut *(r.as_ref() as *const _ as *mut Read) };
+                    let mut buf = vec![0u8; remain];
+                    r.read_exact(&mut buf).ok();
+                }
+            }
+            ChunkReader(ref _r) => unimplemented!(),
+            EmptyReader => {}
+        }
     }
 }
