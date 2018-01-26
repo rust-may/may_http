8dd27fe8294498b10441b1d5d1f76536519837ce
Xudong Huang <huangxu008@hotmail.com>
2018-01-26 14:46:28 +0000

    :pencil: impl chunk writer

--- src/body/body_writer.rs
+++ src/body/body_writer.rs
@@ -37,7 +37,14 @@ impl Write for BodyWriter {
                 *remain -= n;
                 Ok(n)
             }
-            ChunkWriter(ref _w) => unimplemented!(),
+            ChunkWriter(ref w) => {
+                let chunk_size = buf.len();
+                let w = unsafe { &mut *(w.as_ref() as *const _ as *mut Write) };
+                write!(w, "{:X}\r\n", chunk_size)?;
+                w.write_all(buf)?;
+                w.write_all(b"\r\n")?;
+                Ok(chunk_size)
+            }
             EmptyWriter(_) => Ok(0),
             InvalidWriter => unreachable!(),
         }
@@ -75,7 +82,12 @@ impl Drop for BodyWriter {
                 }
                 w.flush().ok();
             }
-            ChunkWriter(ref _w) => unimplemented!(),
+            ChunkWriter(ref w) => {
+                // write the chunk end and flush
+                let w = unsafe { &mut *(w.as_ref() as *const _ as *mut Write) };
+                w.write_all(b"0\r\n\r\n").ok();
+                w.flush().ok();
+            }
             EmptyWriter(ref w) => {
                 let w = unsafe { &mut *(w.as_ref() as *const _ as *mut Write) };
                 w.flush().ok();
