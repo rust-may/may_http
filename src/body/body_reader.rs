7906cded0fd46f8d245ce3ebd6d89388018a2f12
Xudong Huang <huangxu008@hotmail.com>
2018-01-28 02:42:26 +0000

    :gift: impl ChunkReader (#1)

--- src/body/body_reader.rs
+++ src/body/body_reader.rs
@@ -36,7 +36,47 @@ impl Read for BodyReader {
                 *remain -= n;
                 Ok(n)
             }
-            ChunkReader(ref _r, _) => unimplemented!(),
+            ChunkReader(ref r, ref mut opt_remaining) => {
+                let r = unsafe { ::utils::transmute_mut(r.as_ref()) };
+                let mut rem = match *opt_remaining {
+                    Some(ref rem) => *rem,
+                    // None means we don't know the size of the next chunk
+                    None => read_chunk_size(r)?,
+                };
+                trace!("Chunked read, remaining={:?}", rem);
+
+                if rem == 0 {
+                    if opt_remaining.is_none() {
+                        eat(r, b"\r\n")?;
+                    }
+
+                    *opt_remaining = Some(0);
+
+                    // chunk of size 0 signals the end of the chunked stream
+                    // if the 0 digit was missing from the stream, it would
+                    // be an InvalidInput error instead.
+                    trace!("end of chunked");
+
+                    return Ok(0);
+                }
+
+                let to_read = cmp::min(rem, buf.len());
+                let count = r.read(&mut buf[..to_read])?;
+
+                if count == 0 {
+                    *opt_remaining = Some(0);
+                    return Err(io::Error::new(io::ErrorKind::Other, "early eof"));
+                }
+
+                rem -= count;
+                *opt_remaining = if rem > 0 {
+                    Some(rem)
+                } else {
+                    eat(r, b"\r\n")?;
+                    None
+                };
+                Ok(count)
+            }
             EmptyReader => Ok(0),
         }
     }
@@ -44,17 +84,111 @@ impl Read for BodyReader {
 
 impl Drop for BodyReader {
     fn drop(&mut self) {
-        match *self {
-            SizedReader(ref r, remain) => {
-                // read enough data
-                if remain > 0 {
-                    let r = unsafe { ::utils::transmute_mut(r.as_ref()) };
-                    let mut buf = vec![0u8; remain];
-                    r.read_exact(&mut buf).ok();
+        // consume all the chunks
+        let mut buf = vec![0; 4096];
+        loop {
+            match self.read(&mut buf) {
+                Err(e) => {
+                    error!("drop Reader err={}", e);
+                    break;
+                }
+                Ok(n) => {
+                    if n == 0 {
+                        break;
+                    } else {
+                        continue;
+                    }
                 }
             }
-            ChunkReader(ref _r, _) => unimplemented!(),
-            EmptyReader => {}
         }
     }
+}
+
+fn eat(rdr: &mut Read, bytes: &[u8]) -> io::Result<()> {
+    let mut buf = [0];
+    for &b in bytes.iter() {
+        match rdr.read(&mut buf)? {
+            1 if buf[0] == b => {}
+            _ => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Invalid characters found",
+                ))
+            }
+        }
+    }
+    Ok(())
+}
+
+/// Chunked chunks start with 1*HEXDIGIT, indicating the size of the chunk.
+fn read_chunk_size(rdr: &mut Read) -> io::Result<usize> {
+    macro_rules! byte (
+        ($rdr:ident) => ({
+            let mut buf = [0];
+            match $rdr.read(&mut buf)? {
+                1 => buf[0],
+                _ => return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Invalid chunk size line",
+                )),
+            }
+        })
+    );
+    let mut size = 0;
+    let radix = 16;
+    let mut in_ext = false;
+    let mut in_chunk_size = true;
+    loop {
+        match byte!(rdr) {
+            b @ b'0'...b'9' if in_chunk_size => {
+                size *= radix;
+                size += (b - b'0') as usize;
+            }
+            b @ b'a'...b'f' if in_chunk_size => {
+                size *= radix;
+                size += (b + 10 - b'a') as usize;
+            }
+            b @ b'A'...b'F' if in_chunk_size => {
+                size *= radix;
+                size += (b + 10 - b'A') as usize;
+            }
+            b'\r' => match byte!(rdr) {
+                b'\n' => break,
+                _ => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "Invalid chunk size line",
+                    ))
+                }
+            },
+            // If we weren't in the extension yet, the ";" signals its start
+            b';' if !in_ext => {
+                in_ext = true;
+                in_chunk_size = false;
+            }
+            // "Linear white space" is ignored between the chunk size and the
+            // extension separator token (";") due to the "implied *LWS rule".
+            b'\t' | b' ' if !in_ext & !in_chunk_size => {}
+            // LWS can follow the chunk size, but no more digits can come
+            b'\t' | b' ' if in_chunk_size => in_chunk_size = false,
+            // We allow any arbitrary octet once we are in the extension, since
+            // they all get ignored anyway. According to the HTTP spec, valid
+            // extensions would have a more strict syntax:
+            //     (token ["=" (token | quoted-string)])
+            // but we gain nothing by rejecting an otherwise valid chunk size.
+            ext if in_ext => {
+                error!("chunk extension byte={}", ext);
+            }
+            // Finally, if we aren't in the extension and we're reading any
+            // other octet, the chunk size line is invalid!
+            _ => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Invalid chunk size line",
+                ));
+            }
+        }
+    }
+    trace!("chunk size={:?}", size);
+    Ok(size)
 }
