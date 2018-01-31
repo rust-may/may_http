2261a4e5ab4ed9f1c338845593d869798a7561ba
Xudong Huang <huangxu008@hotmail.com>
2018-01-31 05:20:17 +0000

    :rocket: save some memery copy

--- src/server/request.rs
+++ src/server/request.rs
@@ -6,62 +6,65 @@ use std::ops::{Deref, DerefMut};
 
 use httparse;
 use http::header::*;
-use bytes::BytesMut;
 use body::BodyReader;
+use bytes::{Bytes, BytesMut};
 use http::{self, Method, Version};
 
 pub(crate) fn decode(buf: &mut BytesMut) -> io::Result<Option<Request>> {
-    let (req, amt) = {
-        let mut headers = [httparse::EMPTY_HEADER; 64];
-        let mut r = httparse::Request::new(&mut headers);
-        let status = r.parse(buf).map_err(|e| {
-            let msg = format!("failed to parse http request: {:?}", e);
-            io::Error::new(io::ErrorKind::Other, msg)
-        })?;
+    #[inline]
+    fn get_slice(buf: &Bytes, data: &[u8]) -> Bytes {
+        let begin = data.as_ptr() as usize - buf.as_ptr() as usize;
+        buf.slice(begin, begin + data.len())
+    }
 
-        let amt = match status {
-            httparse::Status::Complete(amt) => amt,
-            httparse::Status::Partial => return Ok(None),
-        };
+    let mut headers = [httparse::EMPTY_HEADER; 64];
+    let mut r = httparse::Request::new(&mut headers);
+    let status = r.parse(buf).map_err(|e| {
+        let msg = format!("failed to parse http request: {:?}", e);
+        io::Error::new(io::ErrorKind::Other, msg)
+    })?;
 
-        let version = match r.version {
-            Some(v) => {
-                if v == 0 {
-                    Version::HTTP_10
-                } else {
-                    Version::HTTP_11
-                }
-            }
-            None => Version::HTTP_11,
-        };
-
-        // build the request from the parsing result
-        // this is not a zero memory copy solution
-        // but convinient to be used by framework
-
-        let mut req_builder = http::Request::builder();
-        req_builder
-            .method(r.method.unwrap())
-            .uri(r.path.unwrap())
-            .version(version);
-
-        for header in r.headers.iter() {
-            req_builder.header(header.name, header.value);
+    let bytes = match status {
+        httparse::Status::Complete(amt) => {
+            let buf = unsafe { &mut *(buf as *const _ as *mut BytesMut) };
+            buf.split_to(amt).freeze()
         }
-
-        let req = req_builder
-            .body(BodyReader::EmptyReader)
-            .map(|req| Some(Request(req)))
-            .map_err(|e| {
-                let msg = format!("failed to build http request: {:?}", e);
-                io::Error::new(io::ErrorKind::Other, msg)
-            });
-
-        (req, amt)
+        httparse::Status::Partial => return Ok(None),
     };
 
-    buf.advance(amt);
-    req
+    let version = match r.version {
+        Some(v) => {
+            if v == 0 {
+                Version::HTTP_10
+            } else {
+                Version::HTTP_11
+            }
+        }
+        None => Version::HTTP_11,
+    };
+
+    // build the request from the parsing result
+    // this is not a zero memory copy solution
+    // but convinient to be used by framework
+
+    let mut req_builder = http::Request::builder();
+    req_builder
+            .method(r.method.unwrap())
+            .uri(get_slice(&bytes, r.path.unwrap().as_bytes())) // can be optimized with Bytes
+            .version(version);
+
+    for header in r.headers.iter() {
+        let value = unsafe { HeaderValue::from_shared_unchecked(get_slice(&bytes, header.value)) };
+        req_builder.header(header.name, value);
+    }
+
+    req_builder
+        .body(BodyReader::EmptyReader)
+        .map(|req| Some(Request(req)))
+        .map_err(|e| {
+            let msg = format!("failed to build http request: {:?}", e);
+            io::Error::new(io::ErrorKind::Other, msg)
+        })
 }
 
 /// http server request
